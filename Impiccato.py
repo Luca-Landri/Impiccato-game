2a482f16e577ee69923679a0e3a8827cf504de4a
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-01-17 17:09:56 +0100

    Feat: created some functions for the word
    created functions for censoring, randomize and check if the player guess the word

--- Impiccato.py
+++ Impiccato.py
@@ -11,6 +11,47 @@ def read_csv():
                 filtered.append(row["title"])
     return filtered
 
-Films = read_csv()
+def censure_word(word):
+    censored = ""
+    for letter in word:
+        if letter == " ":
+            censored += " "
+        else:
+            censored += "_"
+        
+    return censored
 
-print(Films)
+def random_word(films):
+    word = random.choice(films)
+    return word
+
+def word_guess(guess):
+    if guess == word:
+        return True
+    else:
+        return False
+    
+
+Films = read_csv()
+word = random_word(Films)
+print(word)
+censored = censure_word(word)
+attemp = 5
+player_guess = ""
+
+
+def game():
+    print("_______________________________________________________")
+    print("Benvenuto nel gioco dell'impiccato!")
+    print("Il gioco consiste nel trovare la parola segreta, lettera per lettera.")
+    print("Se sbagli troppi tentativi, il gioco finisce e il tuo amico verrà impiccato!")
+    print("_______________________________________________________")
+    print("La parola da indovinare è: ", censored)
+    print("Hai ", attemp, " tentativi", "che parola è?")
+    player_guess = input()
+    if word_guess(player_guess):
+        print("Hai indovinato!")
+
+
+
+game()
